d075f673e22bd820c980cd0f2f1411261982c460
LiNKeR <am.usa.to.be@gmail.com>
2018-07-03 20:39:39 +0100

    Initial commit

---
+++ app/src/main/java/gitlib/linkersoft/githublibraryexample/gitty.java
@@ -0,0 +1,15 @@
+package gitlib.linkersoft.githublibraryexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class gitty extends AppCompatActivity {
+
+    //this example uses JitPack
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gitty);
+    }
+}
