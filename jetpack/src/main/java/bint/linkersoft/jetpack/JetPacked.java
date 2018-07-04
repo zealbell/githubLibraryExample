8cd7eb7baf16a6bcca63865a24d9a81d2ad5df75
LiNKeR <am.usa.to.be@gmail.com>
2018-07-04 05:34:51 +0100

    Added JetPacked

---
+++ jetpack/src/main/java/bint/linkersoft/jetpack/JetPacked.java
@@ -0,0 +1,14 @@
+package bint.linkersoft.jetpack;
+
+import android.util.Log;
+
+/**
+ * Created by LiNKeR on 7/4/2018.
+ */
+
+public class JetPacked {
+    JetPacked(){}
+    void ZoomOff(int zoom){
+        Log.i("Zooming","zoomed by"+zoom);
+    }
+}
